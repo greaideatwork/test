0736539c90c1c76578e3d0303d8ab95ac8c9ce56
greaideatwork <greaideatwork@gmail.com>
2016-04-23 11:39:01 +0800

    Update config.inc.php

--- config/config.inc.php
+++ config/config.inc.php
@@ -1,7 +1,7 @@
 <?php
 
-$config['ssh_host']='192.168.1.1';
-$config['ssh_password']='sshpassword';
+$config['ssh_host']='123.206.78.44';
+$config['ssh_password']='abc@123';
 $config['ssh_port']='22';
 $config['ssh_username']='root';
 
